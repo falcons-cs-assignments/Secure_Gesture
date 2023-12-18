51aa776bf5cc4bc48b20d70bd56f6e338a3c967c
omaradel16 <omaradel.2001.oa16@gmail.com>
2023-12-19 01:58:24 +0200

    Hand gesture almost work well, need some modifications

--- hand_gesture/HandTrackingModule.py
+++ hand_gesture/HandTrackingModule.py
@@ -12,8 +12,8 @@ def calc_landmark_list(image, landmarks):
 
     # Keypoint
     for _, landmark in enumerate(landmarks.landmark):
-        landmark_x = min(int(landmark.x * image_width), image_width - 1)
-        landmark_y = min(int(landmark.y * image_height), image_height - 1)
+        landmark_x = int(landmark.x * image_width)
+        landmark_y = int(landmark.y * image_height)
         landmark_z = landmark.z
 
         landmark_point.append([landmark_x, landmark_y, landmark_z])
@@ -39,134 +39,31 @@ def calc_bounding_rect(image, landmarks):
     return [x, y, x + w, y + h]
 
 
-def draw_bounding_rect(image, brect, use_brect=True):
-    if use_brect:
-        # Outer rectangle
-        cv.rectangle(image, (brect[0], brect[1]), (brect[2], brect[3]),
-                     (0, 0, 0), 1)
+def draw_bounding_rect(image, landmark_point, text=''):
 
-    return image
+    brect = calc_bounding_rect(image, landmark_point)
 
+    cv.rectangle(image, (brect[0] - 20, brect[1] - 20),
+                 (brect[2] + 20, brect[3] + 20), (0, 0, 0), 1)
 
-def draw_info_text(image, brect, handedness, hand_sign_text=""):
-    cv.rectangle(image, (brect[0], brect[1]), (brect[2], brect[1] - 22),
-                 (0, 0, 0), -1)
+    if text:
+        cv.rectangle(image, (brect[0] - 20, brect[1] - 20), (brect[2] + 20, brect[1] - 44),
+                     (0, 0, 0), -1)
+        cv.putText(image, text, (brect[0] - 20, brect[1] - 27), cv.FONT_HERSHEY_SIMPLEX,
+                   0.6, (255, 255, 255), 1, cv.LINE_AA)
 
-    info_text = handedness
-    if hand_sign_text != "":
-        info_text = info_text + ':' + hand_sign_text
-    cv.putText(image, info_text, (brect[0] + 5, brect[1] - 4),
-               cv.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 1, cv.LINE_AA)
     return image
 
 
 def draw_landmarks(image, landmark_point):
-    if len(landmark_point) > 0:
-        # Thumb
-        cv.line(image, tuple(landmark_point[2][:-1]), tuple(landmark_point[3][:-1]),
-                (0, 0, 0), 6)
-        cv.line(image, tuple(landmark_point[2][:-1]), tuple(landmark_point[3][:-1]),
-                (255, 255, 255), 2)
-        cv.line(image, tuple(landmark_point[3][:-1]), tuple(landmark_point[4][:-1]),
-                (0, 0, 0), 6)
-        cv.line(image, tuple(landmark_point[3][:-1]), tuple(landmark_point[4][:-1]),
-                (255, 255, 255), 2)
-
-        # Index finger
-        cv.line(image, tuple(landmark_point[5][:-1]), tuple(landmark_point[6][:-1]),
-                (0, 0, 0), 6)
-        cv.line(image, tuple(landmark_point[5][:-1]), tuple(landmark_point[6][:-1]),
-                (255, 255, 255), 2)
-        cv.line(image, tuple(landmark_point[6][:-1]), tuple(landmark_point[7][:-1]),
-                (0, 0, 0), 6)
-        cv.line(image, tuple(landmark_point[6][:-1]), tuple(landmark_point[7][:-1]),
-                (255, 255, 255), 2)
-        cv.line(image, tuple(landmark_point[7][:-1]), tuple(landmark_point[8][:-1]),
-                (0, 0, 0), 6)
-        cv.line(image, tuple(landmark_point[7][:-1]), tuple(landmark_point[8][:-1]),
-                (255, 255, 255), 2)
-
-        # Middle finger
-        cv.line(image, tuple(landmark_point[9][:-1]), tuple(landmark_point[10][:-1]),
-                (0, 0, 0), 6)
-        cv.line(image, tuple(landmark_point[9][:-1]), tuple(landmark_point[10][:-1]),
-                (255, 255, 255), 2)
-        cv.line(image, tuple(landmark_point[10][:-1]), tuple(landmark_point[11][:-1]),
-                (0, 0, 0), 6)
-        cv.line(image, tuple(landmark_point[10][:-1]), tuple(landmark_point[11][:-1]),
-                (255, 255, 255), 2)
-        cv.line(image, tuple(landmark_point[11][:-1]), tuple(landmark_point[12][:-1]),
-                (0, 0, 0), 6)
-        cv.line(image, tuple(landmark_point[11][:-1]), tuple(landmark_point[12][:-1]),
-                (255, 255, 255), 2)
-
-        # Ring finger
-        cv.line(image, tuple(landmark_point[13][:-1]), tuple(landmark_point[14][:-1]),
-                (0, 0, 0), 6)
-        cv.line(image, tuple(landmark_point[13][:-1]), tuple(landmark_point[14][:-1]),
-                (255, 255, 255), 2)
-        cv.line(image, tuple(landmark_point[14][:-1]), tuple(landmark_point[15][:-1]),
-                (0, 0, 0), 6)
-        cv.line(image, tuple(landmark_point[14][:-1]), tuple(landmark_point[15][:-1]),
-                (255, 255, 255), 2)
-        cv.line(image, tuple(landmark_point[15][:-1]), tuple(landmark_point[16][:-1]),
-                (0, 0, 0), 6)
-        cv.line(image, tuple(landmark_point[15][:-1]), tuple(landmark_point[16][:-1]),
-                (255, 255, 255), 2)
-
-        # Little finger
-        cv.line(image, tuple(landmark_point[17][:-1]), tuple(landmark_point[18][:-1]),
-                (0, 0, 0), 6)
-        cv.line(image, tuple(landmark_point[17][:-1]), tuple(landmark_point[18][:-1]),
-                (255, 255, 255), 2)
-        cv.line(image, tuple(landmark_point[18][:-1]), tuple(landmark_point[19][:-1]),
-                (0, 0, 0), 6)
-        cv.line(image, tuple(landmark_point[18][:-1]), tuple(landmark_point[19][:-1]),
-                (255, 255, 255), 2)
-        cv.line(image, tuple(landmark_point[19][:-1]), tuple(landmark_point[20][:-1]),
-                (0, 0, 0), 6)
-        cv.line(image, tuple(landmark_point[19][:-1]), tuple(landmark_point[20][:-1]),
-                (255, 255, 255), 2)
-
-        # Palm
-        cv.line(image, tuple(landmark_point[0][:-1]), tuple(landmark_point[1][:-1]),
-                (0, 0, 0), 6)
-        cv.line(image, tuple(landmark_point[0][:-1]), tuple(landmark_point[1][:-1]),
-                (255, 255, 255), 2)
-        cv.line(image, tuple(landmark_point[1][:-1]), tuple(landmark_point[2][:-1]),
-                (0, 0, 0), 6)
-        cv.line(image, tuple(landmark_point[1][:-1]), tuple(landmark_point[2][:-1]),
-                (255, 255, 255), 2)
-        cv.line(image, tuple(landmark_point[2][:-1]), tuple(landmark_point[5][:-1]),
-                (0, 0, 0), 6)
-        cv.line(image, tuple(landmark_point[2][:-1]), tuple(landmark_point[5][:-1]),
-                (255, 255, 255), 2)
-        cv.line(image, tuple(landmark_point[5][:-1]), tuple(landmark_point[9][:-1]),
-                (0, 0, 0), 6)
-        cv.line(image, tuple(landmark_point[5][:-1]), tuple(landmark_point[9][:-1]),
-                (255, 255, 255), 2)
-        cv.line(image, tuple(landmark_point[9][:-1]), tuple(landmark_point[13][:-1]),
-                (0, 0, 0), 6)
-        cv.line(image, tuple(landmark_point[9][:-1]), tuple(landmark_point[13][:-1]),
-                (255, 255, 255), 2)
-        cv.line(image, tuple(landmark_point[13][:-1]), tuple(landmark_point[17][:-1]),
-                (0, 0, 0), 6)
-        cv.line(image, tuple(landmark_point[13][:-1]), tuple(landmark_point[17][:-1]),
-                (255, 255, 255), 2)
-        cv.line(image, tuple(landmark_point[17][:-1]), tuple(landmark_point[0][:-1]),
-                (0, 0, 0), 6)
-        cv.line(image, tuple(landmark_point[17][:-1]), tuple(landmark_point[0][:-1]),
-                (255, 255, 255), 2)
-
-    # Key Points
-    for index, landmark in enumerate(landmark_point):
-        if index in [4, 8, 12, 16, 20]:
-            cv.circle(image, (landmark[0], landmark[1]), 8, (255, 255, 255), -1)
-            cv.circle(image, (landmark[0], landmark[1]), 8, (0, 0, 0), 1)
-        else:
-            cv.circle(image, (landmark[0], landmark[1]), 5, (255, 255, 255), -1)
-            cv.circle(image, (landmark[0], landmark[1]), 5, (0, 0, 0), 1)
-
+    landmark_drawing_spec=mp.solutions.drawing_utils.DrawingSpec(color=(255, 0, 0))
+    connection_drawing_spec=mp.solutions.drawing_utils.DrawingSpec(color=(255, 0, 0))
+    
+    mp.solutions.drawing_utils.draw_landmarks(image, landmark_point,
+                                              mp.solutions.hands.HAND_CONNECTIONS,
+                                              landmark_drawing_spec=landmark_drawing_spec,
+                                              connection_drawing_spec=connection_drawing_spec
+                                              )
     return image
 
 
@@ -187,58 +84,63 @@ class HandDetector:
             min_tracking_confidence=self.min_tracking_confidence,
             model_complexity=self.model_complexity
         )
-        self.mpDraw = mp.solutions.drawing_utils
 
         self.results = None
 
     def find_hands(self, img, draw=True):
+        """
+        :param img:
+        :param draw:
+        :return: rightHand [[],[],.....[], handLms]
+                to get landmarks rightHand[0,-1]
+                to get normalized landmarks rightHand[-1]
+        """
         imgRGB = cv.cvtColor(img, cv.COLOR_BGR2RGB)
         self.results = self.hands.process(imgRGB)
 
+        rightHand = []
+        leftHand = []
         # calc landmark list
         if self.results.multi_hand_landmarks:
-            all_hands = []  # contains all hands in the image
             for hand_type, hand_landmarks in zip(self.results.multi_handedness, self.results.multi_hand_landmarks):
-                my_hand = {"lmList": calc_landmark_list(img, hand_landmarks),
-                           "type": hand_type.classification[0].label,
-                           "bbox": calc_bounding_rect(img, hand_landmarks)}
+                tempList = calc_landmark_list(img, hand_landmarks)
+                tempList.append(hand_landmarks)
 
-                all_hands.append(my_hand)
-        else:
-            return None
-        return all_hands
-
+                if hand_type.classification[0].label == "Right":
+                    rightHand = tempList
+                else:
+                    leftHand = tempList
+        #
+        return rightHand, leftHand
 
 def find_distance(p1, p2, hand_border, img=None, color=(255, 0, 255), scale=5):
+    """
+    :p1 & p2: should be (x, y)
+    """
     # TODO: solve the problem of distance from the camera
     border_rad = math.hypot(hand_border[0] - hand_border[2], hand_border[1] - hand_border[3])
 
-    x1, y1 = p1[:-1]
-    x2, y2 = p2[:-1]
+    x1, y1 = p1
+    x2, y2 = p2
     cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
     length = math.hypot(x2 - x1, y2 - y1)
     ratio = length / border_rad
-    # info = (x1, y1, x2, y2, cx, cy)
     if img is not None:
         cv.circle(img, (x1, y1), scale, color, cv.FILLED)
         cv.circle(img, (x2, y2), scale, color, cv.FILLED)
         cv.line(img, (x1, y1), (x2, y2), color, max(1, scale // 3))
         cv.circle(img, (cx, cy), scale, color, cv.FILLED)
 
-    return ratio, img    #, info
+    return ratio, img
 
 
 def draw_hand(img, hand, label):
-    brect = hand["bbox"]
-    img = draw_bounding_rect(img, brect)
-    img = draw_landmarks(img, hand["lmList"])
-    img = draw_info_text(img, brect, hand["type"], label)
+    img = draw_bounding_rect(img, hand[-1], label)
+    img = draw_landmarks(img, hand[-1])
     return img
 
 
 def main():
-    pTime = 0
-
     cap = cv.VideoCapture(0)
     cap.set(cv.CAP_PROP_FRAME_WIDTH, 1024)
     cap.set(cv.CAP_PROP_FRAME_HEIGHT, 720)
@@ -247,21 +149,7 @@ def main():
     while True:
         success, img = cap.read()
 
-        hands, img = detector.find_hands(img)
-
-        if hands:
-            # lmList=hands[0]
-            lmList = next((d for d in hands if d.get('type') == 'Right'), None)
-            if lmList:
-                print(lmList)
-
-        cTime = time.time()
-        fps = 1 / (cTime - pTime)
-        pTime = cTime
-
-        cv.putText(img, str(int(fps)), (10, 70), cv.FONT_HERSHEY_PLAIN, 3,
-                    (255, 0, 255), 3)
-
+        
         cv.imshow("Image", img)
         q = cv.waitKey(1)
         if q == ord("q"):
--- hand_gesture/app.py
+++ hand_gesture/app.py
@@ -2,18 +2,31 @@ import cv2 as cv
 import copy
 import itertools
 import csv
-
-# import numpy as np
+import multiprocessing
 
 from model.keypoint_classifier import KeyPointClassifier
-from HandTrackingModule import HandDetector, draw_hand, find_distance
-from control import Speaker
+from HandTrackingModule import HandDetector, draw_hand, find_distance, calc_bounding_rect
+from control import Speaker, Painter, take_screenshot, open_application
 
+# Initiations ###########################################################
 cap_device = 0
 cap_height = 900
-cap_width = cap_height * 1.618
+cap_width = cap_height * (16/9)
 
+# Initiate Subprocess 
+screenshot_process = None
+application_process = None
+
+# FLAGS ###########################################################
 vol_change = False
+drawing_mode = False
+selection = False
+pen = False
+termination = False
+quit = True
+
+hand_flag = False
+# #################################################################
 
 # Read labels ###########################################################
 with open('model/keypoint_classifier_label.csv',
@@ -25,17 +38,25 @@ with open('model/keypoint_classifier_label.csv',
 
 
 def main():
-    global vol_change
+    global screenshot_process, application_process
+    global vol_change, drawing_mode, selection, pen, quit, termination, hand_flag
+
     # Sound Control initiation #############################################
     volume = Speaker()
+    
     # Camera preparation ###############################################################
     cap = cv.VideoCapture(cap_device)
     cap.set(cv.CAP_PROP_FRAME_WIDTH, cap_width)
     cap.set(cv.CAP_PROP_FRAME_HEIGHT, cap_height)
 
+    width = int(cap.get(cv.CAP_PROP_FRAME_WIDTH))
+    height = int(cap.get(cv.CAP_PROP_FRAME_HEIGHT))
+
     # Model load #############################################################
     mp_model = HandDetector()
     keypoint_classifier = KeyPointClassifier()
+
+    painter = Painter(height, width)
 
     #  ####################################################################
     while True:
@@ -50,46 +71,130 @@ def main():
             break
         image = cv.flip(image, 1)  # Mirror display
         debug_image = copy.deepcopy(image)
+            
         # Detection #############################################################
-        hands = mp_model.find_hands(image)
-        if hands:
-            # split right and left hands
-            right_hand = [h for h in hands if h["type"] == "Right"]
-            left_hand = [h for h in hands if h["type"] == "Left"]
+        right_hand, left_hand = mp_model.find_hands(image)
+        
+        if left_hand:
 
-            # #############################
             # classify left hand sign
-            if len(left_hand):
-                landmark_list = left_hand[0]['lmList']
-                # Conversion to relative coordinates / normalized coordinates
-                pre_processed_landmark_list = pre_process_landmark(landmark_list)
-                # Hand sign classification
-                hand_sign_id = keypoint_classifier(pre_processed_landmark_list)
-                print(hand_sign_id, keypoint_classifier_labels[hand_sign_id])
+            landmark_list = left_hand[0:-1]
+            # Conversion to relative coordinates / normalized coordinates
+            pre_processed_landmark_list = pre_process_landmark(landmark_list)
+            # Hand sign classification
+            hand_sign_id = keypoint_classifier(pre_processed_landmark_list)
+            print(hand_sign_id, keypoint_classifier_labels[hand_sign_id])
 
+            if not drawing_mode:
                 # draw part
-                draw_hand(debug_image, left_hand[0], keypoint_classifier_labels[hand_sign_id])
-                # #######################################
-                # control part
-                # TODO: switching depend on left hand_sign_id
-                if hand_sign_id == 0:    # if "start" sign
+                draw_hand(debug_image, left_hand, keypoint_classifier_labels[hand_sign_id])
+            # #######################################
+            # control part
+            # TODO: switching depend on left hand_sign_id
+            if hand_sign_id == keypoint_classifier_labels.index('quit'):  # take screenshot or quit drawing mode
+                if not drawing_mode:
+                    if not screenshot_process or not screenshot_process.is_alive():
+                        screenshot_process = multiprocessing.Process(target=take_screenshot)
+                        screenshot_process.start()
+                
+                else:
+                    drawing_mode = False
+
+            if hand_sign_id == keypoint_classifier_labels.index('open_app'):    # open text editor
+                if not drawing_mode:
+                    # open_application("text editor")
+                    if not application_process or not application_process.is_alive():
+                        application_process = multiprocessing.Process(target=open_application, args=['text editor', ])
+                        application_process.start()
+
+                pen = False
+                selection = False
+                hand_flag = True
+
+            if hand_sign_id == keypoint_classifier_labels.index('start'):    # if "start" sign
+                if not drawing_mode:
                     vol_change = True   # enable volume change
-                if hand_sign_id == 1:   # if "stop" sign
+
+                pen = False
+                selection = False
+                hand_flag = True
+           
+            if hand_sign_id == keypoint_classifier_labels.index('lock'):   # if "stop" sign
+                if not drawing_mode:
                     vol_change = False  # disable volume change
+                
+                pen = False
+                selection = False
+                hand_flag = True
 
-            # control sound
-            if vol_change and len(right_hand):
-                right_hand_lm = right_hand[0]["lmList"]
-                right_hand_border = right_hand[0]["bbox"]
+            if hand_sign_id == keypoint_classifier_labels.index('draw'):  # enter drawing mode
+                drawing_mode = True
+                pen = False
+                selection = False
+                hand_flag = True
 
-                distance, debug_image = find_distance(right_hand_lm[4], right_hand_lm[8], right_hand_border, debug_image)
-                vol = volume(distance)
-                print(round(distance, 4), ":", vol)
-            # ##############################################
+            if hand_sign_id == keypoint_classifier_labels.index('select'):  # select colors
+                selection = True
+                pen = False
 
+            if hand_sign_id == keypoint_classifier_labels.index('pen'):  # draw
+                pen = True
+                selection = False
+
+            if hand_sign_id == keypoint_classifier_labels.index('terminate'):  # close program
+                # if drawing_mode:
+                #     drawing_mode = False
+                # else:
+                termination = True
+                pen = False
+                selection = False
+
+        # control sound
+        if vol_change and right_hand:
+            right_hand_lm = right_hand[0:-1]
+            right_hand_border = calc_bounding_rect(image, right_hand[-1])  # right_hand[0]["bbox"]
+
+            distance, debug_image = find_distance(right_hand_lm[4][:-1], right_hand_lm[8][:-1], right_hand_border, debug_image)
+            vol = volume(distance)
+            print(round(distance, 4), ":", vol)
+            cv.putText(debug_image, f"volume: {int(vol)}%", (int(width*0.7), int(height*0.96)), cv.FONT_HERSHEY_COMPLEX,
+                       0.8, (255, 255, 0), 1, cv.LINE_AA)
+        # ###################################################################################
+        # drawing mode
+        if drawing_mode:
+            painter.show_header(debug_image)
+            if right_hand or left_hand:
+                lmList = right_hand[0:-1] or left_hand[0:-1]
+                x1, y1 = lmList[8][:-1]
+
+                if selection:
+                    debug_image = painter.selection(debug_image, x1, y1)
+                
+                if pen:
+                    debug_image = painter.drawing(debug_image, x1, y1, hand_flag)
+                    hand_flag = False
+            else:
+                hand_flag = True
+            
+            debug_image = painter.paint(debug_image)
+        # ###################################################################################
+            
+        # Display ###########################################################################
+            
+        # Show text
+        if drawing_mode:
+            cv.putText(debug_image, "Drawing mode", (int(width*0.02), int(height*0.96)), cv.FONT_HERSHEY_COMPLEX,
+                       1, (255, 255, 0), 2, cv.LINE_AA)
+        else:
+            cv.putText(debug_image, "Control mode", (int(width*0.02), int(height*0.96)), cv.FONT_HERSHEY_COMPLEX,
+                       1, (255, 255, 0), 2, cv.LINE_AA)
+            
+        # Show window             
+        # if drawing_mode:
+        #     painter.show_windows()
         cv.imshow('Hand Gesture Recognition', debug_image)
 
-        #  ####################################################################
+        # ###################################################################################
 
 
 def pre_process_landmark(landmark_list):
